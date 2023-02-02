c2c96f9ccdf09a0b1c74cf5dfb120a9846ae4f52
fullstackltd <fullstackltd@gmail.com>
2023-02-02 13:12:43 +0600

    main change

--- src/root/Main.java
+++ src/root/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test Project");
 		System.out.println("Test Project 2");
+		System.out.println("Test Project 3");
 
 	}
 
